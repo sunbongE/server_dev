ef39ee585afe6f513c7b6877699bbcfddf3a52c0
taehopark <qkrxogh7@gmail.com>
2023-01-13 21:32:49 +0900

    login->유저정보받기

--- login/views.py
+++ login/views.py
@@ -9,33 +9,50 @@ from django.contrib.auth.hashers import make_password, check_password
 
 class AppLogin(APIView):
     def post(self, request):
-        user_id = request.data.get('user_id')
-        user_pw = request.data.get('user_pw')
+        user_id = request.data.get('user_id',"")
+        user_pw = request.data.get('user_pw',"")
         user = LoginUser.objects.filter(user_id=user_id).first()
 
         if user is None:
             return Response(dict(msg='해당 사용자 없음'))
         
         if check_password(user_pw, user.user_pw):
-            return Response(dict(msg='로그인 성공'))
+            return Response(dict(msg='로그인 성공',
+            user_id=user.user_id,
+            user_pw=user.user_pw, 
+            birth_day=user.birth_day,
+            gender=user.gender,
+            email=user.email,
+            name=user.name,
+            age=user.age))
         else:
             return Response(dict(msg='로그인 실패, 비번틀림'))
 
 class RegistUser(APIView):
     def post(self,request):
-        user_id = request.data.get('user_id')
-        user_pw = request.data.get('user_pw')
-        user_pw_encryted = make_password(user_pw)
+        user_id = request.data.get('user_id','')
+        user_pw = request.data.get('user_pw','')
+        birth_day=request.data.get('birth_day',None)
+        gender=request.data.get('gender','male')
+        email=request.data.get('email','')
+        name=request.data.get('name','')
+        age=request.data.get('age',20)
 
+        user_pw_encryted = make_password(user_pw) # 암호화
         user = LoginUser.objects.filter(user_id=user_id).first()
         if user is not None:
             Response(dict(msg='동일한 아이디가 있습니다.'))
         #암호화된 비밀번호로 저장한다.
-        LoginUser.objects.create(user_id=user_id, user_pw=user_pw_encryted)
+        LoginUser.objects.create(user_id=user_id, user_pw=user_pw_encryted, birth_day=birth_day,gender=gender,email=email,name=name,age=age)
 
         data = dict(
             user_id=user_id,
-            user_pw=user_pw_encryted
+            user_pw=user_pw_encryted, 
+            birth_day=birth_day,
+            gender=gender,
+            email=email,
+            name=name,
+            age=age,
         )
 
         return Response(data)
